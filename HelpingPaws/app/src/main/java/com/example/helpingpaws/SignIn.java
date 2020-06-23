38d686a4c2984e297cc95712e20009974fbf3112
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-06-23 23:21:51 +0530

    sign in page

--- HelpingPaws/app/src/main/java/com/example/helpingpaws/SignIn.java
+++ HelpingPaws/app/src/main/java/com/example/helpingpaws/SignIn.java
@@ -3,12 +3,54 @@ package com.example.helpingpaws;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignIn extends AppCompatActivity {
+
+    EditText edtPhone,edtPassword;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        edtPassword=(MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone=(MaterialEditText)findViewById(R.id.edtPhone);
+
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+
+        //Init firebase
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        final DatabaseReference table_user=database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        //Get user information
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 }
