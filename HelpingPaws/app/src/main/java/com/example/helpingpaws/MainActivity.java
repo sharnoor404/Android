dd470776e03b41950ffc3dd3f79ddb3027ad46bd
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-06-24 10:45:26 +0530

    sign in page

--- HelpingPaws/app/src/main/java/com/example/helpingpaws/MainActivity.java
+++ HelpingPaws/app/src/main/java/com/example/helpingpaws/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.helpingpaws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn=new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
             }
         });
     }
--- HelpingPaws/app/src/main/java/com/example/helpingpaws/SignIn.java
+++ HelpingPaws/app/src/main/java/com/example/helpingpaws/SignIn.java
@@ -2,11 +2,14 @@ package com.example.helpingpaws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.helpingpaws.Model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,13 +39,34 @@ public class SignIn extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final ProgressDialog mDialog=new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please wait");
+                mDialog.show();
+
                 table_user.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
 
-                        //Get user information
-
-
+                        //Check if user does not exist in the database
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                            //Get user information
+                            mDialog.dismiss();
+                            User user=dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                            if(user.getPassword().equals(edtPassword.getText().toString()))
+                            {
+                                Toast.makeText(SignIn.this, "Sign In Successful!", Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(SignIn.this, "Sign In Failed! Please check your phone number and password and try again.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "The user does not Exist. Please Signup first.", Toast.LENGTH_SHORT).show();
+                        }
                     }
 
                     @Override
