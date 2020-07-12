824a602545392e8ccd889929bf98312a510de6e5
Manvikaul <47503962+Manvikaul@users.noreply.github.com>
2020-07-12 22:29:44 +0530

    updates

--- HelpingPaws/app/src/main/java/com/example/helpingpaws/PetList.java
+++ HelpingPaws/app/src/main/java/com/example/helpingpaws/PetList.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.helpingpaws.Model.Pets;
+import com.example.helpingpaws.ViewHolder.PetViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class PetList extends AppCompatActivity {
 
@@ -16,6 +20,8 @@ public class PetList extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference petList;
+    String categoryId="";
+    FirebaseRecyclerAdapter<Pets, PetViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,27 @@ public class PetList extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         //Get intent here
-        
+        if(getIntent()!=null)
+        {
+            categoryId=getIntent().getStringExtra("CategoryId");
+        }
+        if(!categoryId.isEmpty() &&  categoryId!=null)
+        {
+            loadListPet(categoryId);
+        }
+
+    }
+
+    private void loadListPet(String categoryId) {
+        adapter=new FirebaseRecyclerAdapter<Pets, PetViewHolder>(Pets.class,
+                R.layout.pet_option,
+                PetViewHolder.class, petList.orderByChild("MenuId").equalTo(categoryId)) {
+            @Override
+            protected void populateViewHolder(PetViewHolder petViewHolder, Pets pets, int i) {
+                petViewHolder.pet_name.setText(pets.getName());
+                Picasso.with(getBaseContext()).load(pets.getImage())
+                        .into(petViewHolder.pet_image);
+            }
+        };
     }
 }
